d6b63765d9bb01db6e7b53201ab788df80ac531b
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2024-06-17 21:45:44 +0530

    bug fix

--- make_space/src/main/java/com/example/geektrust/service/ApplicationService.java
+++ make_space/src/main/java/com/example/geektrust/service/ApplicationService.java
@@ -19,20 +19,25 @@ public class ApplicationService {
             String ip = sc.nextLine();
             String[] tokens = ip.split(" ");
             //System.out.println(ip);
+            String startTime = tokens[1];
+            String endTime = tokens[2];
             if(tokens[0].equals("VACANCY")){
-                List<String> vacancies = bookingService.vacancies(tokens[1],tokens[2]);
-                if(vacancies.size()==0) {
-                    System.out.println("NO_VACANT_ROOM");
+                if(!timestampSyntaxChecker.isValidTimestamps(startTime,endTime)) {
+                    System.out.println("INCORRECT_INPUT");
                 }
                 else {
-                    for (String room : vacancies)
-                        System.out.printf(room + " ");
-                    System.out.println();
+                    List<String> vacancies = bookingService.vacancies(startTime, endTime);
+                    if (vacancies.size() == 0) {
+                        System.out.println("NO_VACANT_ROOM");
+                    } else {
+                        for (String room : vacancies)
+                            System.out.printf(room + " ");
+                        System.out.println();
+                    }
                 }
             }
             else if(tokens[0].equals("BOOK")){
-                String startTime = tokens[1];
-                String endTime = tokens[2];
+
                 Integer persons = Integer.parseInt(tokens[3]);
 
                 if(!timestampSyntaxChecker.isValidTimestamps(startTime,endTime))
